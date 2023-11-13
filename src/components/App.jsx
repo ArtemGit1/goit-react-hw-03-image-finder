0aa79e3b09d08d94160c028dea93ec1deb5fcd1c
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-11-13 15:38:57 +0200

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import './styles.css';
 import Searchbar from 'components/Searchbar/Searchbar.js';
 import ImageGallery from 'components/ImageGallery/ImageGallery.js';
 import Button from 'components/Button/Button.js';
 import Loader from 'components/Loader/Loader.js';
 import Modal from 'components/Modal/Modal.js';
-
-const API_KEY = '39751555-c2fbc931ac716611d03f33f4d';
+import { getImages } from 'components/Services/Api.js';
 
 class App extends Component {
   constructor(props) {
@@ -26,14 +23,7 @@ class App extends Component {
     const { searchQuery, page } = this.state;
     try {
       this.setState({ loading: true });
-      const response = await axios.get(
-        `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      );
-      const newImages = response.data.hits.map((image) => ({
-        id: image.id,
-        webformatURL: image.webformatURL,
-        largeImageURL: image.largeImageURL,
-      }));
+      const newImages = await getImages(searchQuery, page);
       this.setState((prevState) => ({ images: [...prevState.images, ...newImages] }));
     } catch (error) {
       console.error('Error fetching images', error);
@@ -69,8 +59,8 @@ class App extends Component {
 
         {loading && <Loader />}
 
-        {images.length > 0 && !loading && <Button onLoadMore={this.loadMore} />}
-
+        {!loading && images.length > 0 && <Button onLoadMore={this.loadMore} />}
+        
         {showModal && <Modal onClose={this.closeModal} largeImageURL={selectedImage} />}
       </div>
     );
